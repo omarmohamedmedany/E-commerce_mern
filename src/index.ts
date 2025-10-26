af43afdad810f9291cf48232031d9921af919a74
omar <168734552+omarmohamedmedany@users.noreply.github.com>
2025-10-26 10:44:11 +0300

    product feature

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import userRoute from "./routes/userRoute.ts";
+import productRoute from "./routes/productRoute.ts";
+import { seedIntialProducts } from "./services/productService.ts";
 
 const app = express();
 const port = 3001;
@@ -12,7 +14,11 @@ mongoose
   .then(() => console.log("mongo connected!"))
   .catch((err) => console.log("failed to connect ", err));
 
+  //seed the products to database 
+  seedIntialProducts();
+
   app.use('/user', userRoute);
+  app.use('/products', productRoute);
 
   app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
